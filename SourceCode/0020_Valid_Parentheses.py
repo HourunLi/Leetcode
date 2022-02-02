313eba23408cf1f0804aafcd39769890689f9076
Andy <2019202463@ruc.edu.cn>
2022-02-02 10:00:42 +0800

    20 valid parentheses and python solution

---
+++ SourceCode/0020_Valid_Parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s):
+        if len(s) % 2 == 1:
+            return False
+
+        pairs = {
+            ")": "(",
+            "]": "[",
+            "}": "{",
+        }
+        stack = []
+        for ch in s:
+            if ch in pairs:
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return not stack
