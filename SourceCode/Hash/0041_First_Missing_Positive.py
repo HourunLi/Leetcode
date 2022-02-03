df6703cf0f8cd0d7c78b586fec1b2ca2386c8d06
Andy <2019202463@ruc.edu.cn>
2022-02-03 17:14:59 +0800

    41 hash python solution

---
+++ SourceCode/Hash/0041_First_Missing_Positive.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstMissingPositive(self, nums):
+        n = len(nums)
+        for i in range(n):
+            if nums[i] <= 0:
+                nums[i] = n+1
+        for i in range(n):
+            num = abs(nums[i])
+            if num <= n:
+                nums[num-1] = -abs(nums[num-1])
+
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+
+        return n+1
