2a59601d962c3de8bf20cace2e1bbffa924679d2
Andy <lhr325803176@163.com>
2022-02-05 16:36:49 +0800

    212 another tricky method

--- SourceCode/Trie/0212_Word_Search_II.cpp
+++ SourceCode/Trie/0212_Word_Search_II.cpp
@@ -91,3 +91,95 @@ public:
     return ret;
   }
 };
+
+
+// another tricky method
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TrieNode {
+  string word;
+  TrieNode *children[26];
+  int cnt; // the number of children
+
+  TrieNode () {
+    word = "";
+    memset(children, 0, 26 * sizeof(TrieNode*));
+    cnt = 0;
+  }
+};
+
+class Trie {
+public:
+  TrieNode *root;
+  Trie() {
+    root = new TrieNode();
+  }
+  void insert(string word) {
+    TrieNode *ptr = root;
+    for(auto &i : word) {
+      if(ptr->children[i-'a'] == nullptr) {
+        ptr->children[i-'a'] = new TrieNode();
+        ptr->cnt++;
+      }
+      ptr = ptr->children[i-'a']; 
+    }
+    ptr->word = word;
+    return;
+  }
+};
+
+
+class Solution {
+private:
+  int dx[4] = {1, 0, -1, 0};
+  int dy[4] = {0, 1, 0, -1};
+  int n, m;
+public:
+  void dfs(int x, int y, vector<vector<char>>& board, TrieNode *root, vector<string> &res) {
+    char ch = board[x][y];
+    if(root == nullptr || root->children[ch-'a'] == nullptr) {
+      return;
+    }
+    TrieNode *next = root->children[ch-'a'];
+    if(next->word.size() > 0) {
+      // cout << next->word << endl;
+      res.push_back(next->word);
+      next->word = "";
+    }
+
+    board[x][y] = '#';
+    for(int i = 0; i < 4; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+      if(nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == '#') {
+        continue;
+      }
+      dfs(nx, ny, board, next, res);
+    }
+    board[x][y] = ch;
+    if(next->cnt == 0) {
+      delete next;
+      root->children[ch-'a'] = nullptr;
+      root->cnt--;
+    }
+    return;
+  }
+
+  vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+    Trie *trie = new Trie();
+    vector<string> ret;
+
+    for(string word: words) {
+      trie->insert(word);
+    }
+
+    n = board.size(), m = board[0].size();
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        dfs(i, j, board, trie->root, ret);
+      }
+    }
+    return ret;
+  }
+};
---
+++ SourceCode/Trie/0212_Word_Search_II.py
@@ -0,0 +1,49 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.word = ""
+
+    def insert(self, word):
+        ptr = self
+        for ch in word:
+            if ch not in self.children:
+                self.children[ch] = Trie()
+            ptr = self.children[ch]
+        ptr.word = word
+
+
+class Solution(object):
+    def findWords(self, board, words):
+        dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+        n, m = len(board), len(board[0])
+        trie = Trie()
+        ans = []
+        for word in words:
+            trie.insert(word)
+
+        def dfs(x, y, trieNode):
+            # child func can use var from parent
+            ch = board[x][y]
+            if ch not in trieNode:
+                return
+
+            next = trieNode.children[ch]
+            if next.word != "":
+                ans.append(next.word)
+                next.word = ""
+
+            board[x][y] = '#'
+            for i in range(4):
+                nx, ny = x+dx[i], y+dy[i]
+                if nx < 0 or ny < 0 or nx >= n or ny >= m or board[nx][ny] == '#':
+                    continue
+                dfs(nx, ny, next)
+            board[x][y] = ch
+            if not next.children:
+                trieNode.children.pop(ch)
+
+        for i in range(n):
+            for j in range(m):
+                dfs(i, j, trie)
+
+        return ans
